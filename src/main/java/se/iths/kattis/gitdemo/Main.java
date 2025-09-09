c87d5df17d179f2cd31d83f5aeb63240cc55c87d
Kattis82 <kattiscalmvik@outlook.com>
2025-09-09 09:15:29 +0200

    Ändrade i Main...

--- src/main/java/se/iths/kattis/gitdemo/Main.java
+++ src/main/java/se/iths/kattis/gitdemo/Main.java
@@ -1,4 +1,10 @@
 package se.iths.kattis.gitdemo;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+
+        System.out.println("Goodbye");
+    }
 }
